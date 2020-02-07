219496add4fe503981d6046d5d7f09968a44701b
Vitaly Cheptsov <cheptsov@ispras.ru>
2020-02-07 18:35:19 +0300

    lab3: initial version

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -6,6 +6,104 @@
 
 #include <kern/kdebug.h>
 
+extern const struct Stab __STAB_BEGIN__[];	// Beginning of stabs table
+extern const struct Stab __STAB_END__[];	// End of stabs table
+extern const char __STABSTR_BEGIN__[];		// Beginning of string table
+extern const char __STABSTR_END__[];		// End of string table
+
+
+// stab_binsearch(stabs, region_left, region_right, type, addr)
+//
+//	Some stab types are arranged in increasing order by instruction
+//	address.  For example, N_FUN stabs (stab entries with n_type ==
+//	N_FUN), which mark functions, and N_SO stabs, which mark source files.
+//
+//	Given an instruction address, this function finds the single stab
+//	entry of type 'type' that contains that address.
+//
+//	The search takes place within the range [*region_left, *region_right].
+//	Thus, to search an entire set of N stabs, you might do:
+//
+//		left = 0;
+//		right = N - 1;     /* rightmost stab */
+//		stab_binsearch(stabs, &left, &right, type, addr);
+//
+//	The search modifies *region_left and *region_right to bracket the
+//	'addr'.  *region_left points to the matching stab that contains
+//	'addr', and *region_right points just before the next stab.  If
+//	*region_left > *region_right, then 'addr' is not contained in any
+//	matching stab.
+//
+//	For example, given these N_SO stabs:
+//		Index  Type   Address
+//		0      SO     f0100000
+//		13     SO     f0100040
+//		117    SO     f0100176
+//		118    SO     f0100178
+//		555    SO     f0100652
+//		556    SO     f0100654
+//		657    SO     f0100849
+//	this code:
+//		left = 0, right = 657;
+//		stab_binsearch(stabs, &left, &right, N_SO, 0xf0100184);
+//	will exit setting left = 118, right = 554.
+//
+static void
+stab_binsearch(const struct Stab *stabs, int *region_left, int *region_right,
+	       int type, uintptr_t addr)
+{
+	int l = *region_left, r = *region_right, any_matches = 0;
+
+	while (l <= r) {
+		int true_m = (l + r) / 2, m = true_m;
+
+		// search for earliest stab with right type
+		while (m >= l && stabs[m].n_type != type)
+			m--;
+		if (m < l) {	// no match in [l, m]
+			l = true_m + 1;
+			continue;
+		}
+
+		// actual binary search
+		any_matches = 1;
+		if (stabs[m].n_value < addr) {
+			*region_left = m;
+			l = true_m + 1;
+		} else if (stabs[m].n_value > addr) {
+			*region_right = m - 1;
+			r = m - 1;
+		} else {
+			// exact match for 'addr', but continue loop to find
+			// *region_right
+			*region_left = m;
+			l = m;
+			addr++;
+		}
+	}
+
+	if (!any_matches)
+		*region_right = *region_left - 1;
+	else {
+		// find rightmost region containing 'addr'
+		for (l = *region_right;
+		     l > *region_left && stabs[l].n_type != type;
+		     l--)
+			/* do nothing */;
+		*region_left = l;
+	}
+}
+
+uintptr_t
+stabs_find_function(const char * const fname)
+{
+	// const struct Stab *stabs = __STAB_BEGIN__, *stab_end = __STAB_END__;
+	// const char *stabstr = __STABSTR_BEGIN__, *stabstr_end = __STABSTR_END__;
+
+	//LAB 3: Your code here.
+	return 0;
+}
+
 // debuginfo_eip(addr, info)
 //
 //	Fill in the 'info' structure with information about the specified
@@ -70,4 +168,24 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 		return code;
 	}
 	return 0;
+}
+
+uintptr_t
+dwarf_find_function(const char * const fname)
+{
+	// There are two functions for function name lookup.
+	// address_by_fname, which looks for function name in section .debug_pubnames
+	// and naive_address_by_fname which performs full traversal of DIE tree.
+	// LAB3: Your code here
+	return 0;
+}
+
+uintptr_t
+find_function(const char *const fname)
+{
+	uintptr_t address = stabs_find_function(fname);
+	if (address == 0) {
+		address = dwarf_find_function(fname);
+	}
+	return address;
 }
--- kern/monitor.c
+++ kern/monitor.c
@@ -5,6 +5,7 @@
 #include <inc/string.h>
 #include <inc/memlayout.h>
 #include <inc/assert.h>
+#include <inc/dwarf.h>
 #include <inc/x86.h>
 
 #include <kern/console.h>
